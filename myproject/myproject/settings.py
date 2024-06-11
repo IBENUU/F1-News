569cc3c72f9ecf4be781bb83128a2668bc6544a3
ibnu <bintangrahore@gmail.com>
2024-06-11 20:00:43 +0800

    update

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -99,6 +99,10 @@ DATABASES = {
         'PASSWORD': config('DB_PASSWORD'),
         'HOST': config('DB_HOST'),
         'PORT': config('DB_PORT'),
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        }
     }
 }
 
